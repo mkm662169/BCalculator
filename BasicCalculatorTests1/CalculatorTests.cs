1b2be4aa523c14acf8b1aed434ef8839722cdce9
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-09 16:35:25 +0200

    Add project files.

---
+++ BasicCalculatorTests1/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        private Calculator Calculator = new Calculator();
+        [TestMethod()]
+        public void AddTest()
+        {
+            int c = Calculator.Add(30, 47);
+
+            Assert.AreEqual(77, c);
+            Assert.AreEqual(77, Calculator.solution);
+        }
+    }
+}
---
+++ operationsTests/AdditionTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using operations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operations.Tests
+{
+    [TestClass()]
+    public class AdditionTests
+    {
+        private readonly int a = 5;
+        private readonly int b = 10;
+        private readonly double c = 5.7;
+        private readonly double d = 4.5;
+        private readonly int[] arrayA = { 1, 2, 3, 4 };
+        private readonly double[] arrayB = { 1.1, 2.2, 3.3, 4.2 };
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(15, Addition.Add(a, b));
+        }
+
+        [TestMethod()]
+        public void AddDoubleTest()
+        {
+            Assert.AreEqual(10.2, Addition.Add(c, d));
+        }
+
+        [TestMethod()]
+        public void AddArrayTest()
+        {
+            Assert.AreEqual(10, Addition.Add(arrayA));
+        }
+
+        [TestMethod()]
+        public void AddDoubleArrayTest()
+        {
+            Assert.AreEqual(10.8, Addition.Add(arrayB));
+        }
+    }
+}
