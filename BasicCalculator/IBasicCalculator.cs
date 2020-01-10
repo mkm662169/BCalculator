b4e9e427bc2c47f09bfe760082b2852c7b4c8ddd
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-10 19:15:57 +0200

    Task: Refactor of BasicCalculator

--- BasicCalculator/IBasicCalculator.cs
+++ BasicCalculator/IBasicCalculator.cs
@@ -7,5 +7,17 @@ namespace BasicCalculator
     public interface IBasicCalculator
     {
         public dynamic Add(dynamic a, dynamic b);
+
+        public dynamic Cubing(dynamic a);
+
+        public dynamic Difference(dynamic a, dynamic b);
+
+        public dynamic Multiply(dynamic a, dynamic b);
+
+        public dynamic Quotient(dynamic a, dynamic b);
+
+        public dynamic Square(dynamic a);
+
+        public dynamic SquareRoot(dynamic a);
     }
 }
