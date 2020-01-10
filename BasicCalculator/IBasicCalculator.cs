f8a2f23e24fc6017dd5b52837d81b637833fffe0
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-10 19:07:30 +0200

    Task: Refactor of interface for BasicCalculator

---
+++ BasicCalculator/IBasicCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculator
+{
+    public interface IBasicCalculator
+    {
+        public dynamic Add(dynamic a, dynamic b);
+    }
+}
