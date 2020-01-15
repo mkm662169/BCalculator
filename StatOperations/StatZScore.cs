175a78cefecc9ab1a0449f8c7ea6de3e3e4a7380
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-15 14:54:48 +0200

    Task: Added Z Score & tests

---
+++ StatOperations/StatZScore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StatOperations;
+using operations;
+using Helpers;
+
+namespace StatOperations
+{
+    public class StatZScore
+    {
+        public static double ZScore(dynamic score, dynamic mean, dynamic stdDev)
+        {
+            var zScore = Division.Divide(Subtraction.Minus(score, mean), stdDev);
+            return zScore;
+        }
+    }
+}
---
+++ ZScore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZScore
+{
+    public class Class1
+    {
+    }
+}
